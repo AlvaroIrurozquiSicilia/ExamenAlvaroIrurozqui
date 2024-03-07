570e4dfe1c698e7385dfc56efa101652a8d3b6ec
alvaro <alvarois05@educastur.es>
2024-03-07 17:31:50 +0100

    CommitFinal

--- src/app/cronometro/cronometro.component.ts
+++ src/app/cronometro/cronometro.component.ts
@@ -9,16 +9,24 @@ export class CronometroComponent {
   seconds:number=0;
   minutes:number=0;
   hours:number=0;
-  sumar(){
-    this.seconds++;
-    if (this.seconds >=60){
-      this.seconds = 0;
-      this.minutes++;
-    } 
-    if(this.minutes >= 60){
-      this.minutes = 0;
-      this.hours++;
+  stop:boolean=false;
+  sum(){
+    this.stop = true
+    if(this.stop == true){
+      this.seconds++;
+      if (this.seconds >=60){
+        this.seconds = 0;
+        this.minutes++;
+      } 
+      if(this.minutes >= 60){
+        this.minutes = 0;
+        this.hours++;
+      }
+      setTimeout(() => this.sum(), 1000);
     }
-    setTimeout(() => this.sumar(), 1000);
+    
+  }
+  pause(){
+    this.stop=false
   }
 }
